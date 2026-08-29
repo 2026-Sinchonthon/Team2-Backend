c33c93140e7219279c7e7c5791f44f430e305b75
tnbean <yoonsubin35@naver.com>
2026-08-29 10:46:27 +0900

    Setup

---
+++ Team2-Backend/src/main/java/org/example/team2backend/Team2BackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.team2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team2BackendApplication.class, args);
+    }
+
+}
